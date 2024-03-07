ce340bd828a9e3f5136c1f715f840bc943523e71
yoissy <33407132+yoissy@users.noreply.github.com>
2024-03-07 12:47:57 -0500

    Initializing

---
+++ material-ui-nextjs-ts/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import Dashboard from "@/components/Dashboard";
+
+// `app/page.tsx` is the UI for the `/` URL
+export default function Page() {
+    return <Dashboard></Dashboard>
+  }
