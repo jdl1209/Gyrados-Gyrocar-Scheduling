b27f987f9f6aa002ab19a431e4bd3544a36c6d20
yoissy <33407132+yoissy@users.noreply.github.com>
2024-03-11 13:43:01 -0400

    Created basic contact page

---
+++ material-ui-nextjs-ts/src/app/contact/page.tsx
@@ -0,0 +1,35 @@
+//this is needed because some mui functions expecet "client" things like being able to use hooks.
+'use client'
+
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import NextLink from 'next/link';
+import ProTip from '@/components/ProTip';
+import Copyright from '@/components/Copyright';
+import ResponsiveAppBar from '@/components/ResponseiveAppBar';
+
+export default function Contact() {
+  return (
+    <React.Fragment>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          This is the Contact Page!!
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </React.Fragment>
+  );
+}
