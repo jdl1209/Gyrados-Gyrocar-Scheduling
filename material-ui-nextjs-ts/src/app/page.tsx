5cc472f81a81911067f6f85cae09cf6df65eb328
yoissy <33407132+yoissy@users.noreply.github.com>
2024-03-14 12:24:44 -0400

    remove protip and link to about page

--- material-ui-nextjs-ts/src/app/page.tsx
+++ material-ui-nextjs-ts/src/app/page.tsx
@@ -7,7 +7,6 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import NextLink from "next/link";
-import ProTip from "@/components/Dashboard Components/ProTip";
 import Copyright from "@/components/Dashboard Components/Copyright";
 import ResponsiveAppBar from "@/components/ResponseiveAppBar";
 import FAQ from "@/components/FAQ";
@@ -31,10 +30,6 @@ export default function Home() {
         <Testimonials />
         <AboutUs />
         <FAQ />
-        <Link href="/about" color="secondary" component={NextLink}>
-          Go to the about page
-        </Link>
-        <ProTip />
         <Copyright />
       </Box>
     </React.Fragment>
