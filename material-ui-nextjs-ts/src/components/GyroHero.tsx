468c1a846271b203292e4c3ca259c5eda59924ff
kevros167 <56698152+kevros167@users.noreply.github.com>
2024-03-14 15:17:03 -0400

    Fixed Landing page

--- material-ui-nextjs-ts/src/components/GyroHero.tsx
+++ material-ui-nextjs-ts/src/components/GyroHero.tsx
@@ -14,20 +14,20 @@ export default function ProductHero() {
       <Paper
         sx={{
           bgcolor: "#34adad",
-          height: "50vh",
+          height: "60vh",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+          //justifyContent: "center",
+          //alignItems: "left",
         }}
       >
         <Typography
           component="h2"
           variant="h1"
           sx={{
-            width: { sm: "100%", md: "87%" },
-            height: { sm: "100%", md: "78%" },
-            textAlign: { sm: "left", md: "left" },
+            width: { sm: "100%", md: "39%" },
+            height: { sm: "100%", md: "98%" },
+            textAlign: { sm: "center", md: "center" },
             fontWeight: "medium",
           }}
         >
@@ -36,38 +36,77 @@ export default function ProductHero() {
         <Typography
           variant="h4"
           sx={{
-            width: { sm: "100%", md: "87%" },
-            height: { sm: "100%", md: "78%" },
-            textAlign: { sm: "left", md: "left" },
+            width: { sm: "100%", md: "21.3%" },
+            height: { sm: "100%", md: "98%" },
+            textAlign: { sm: "center", md: "center" },
           }}
         >
           It’s easy.
-          <br /> It’s convenient.
-          <br /> It’s cost effective.
-          <br />
         </Typography>
         <Typography
           variant="h4"
           sx={{
-            textAlign: { sm: "left", md: "left" },
+            width: { sm: "100%", md: "27%" },
+            height: { sm: "100%", md: "100%" },
+            textAlign: { sm: "left", md: "center" },
+          }}
+        >
+          It’s convenient.
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{
+            width: { sm: "100%", md: "29.4%" },
+            height: { sm: "100%", md: "98%" },
+            textAlign: { sm: "center", md: "center" },
+          }}
+        >
+          It’s cost effective.
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{
+            width: { sm: "100%", md: "23.2%" },
+            height: { sm: "100%", md: "100%" },
+            textAlign: { sm: "left", md: "center" },
             fontStyle: "italic",
             fontWeight: "medium",
           }}
         >
           GyroGoGo!
         </Typography>
+        <Button
+          style={{
+            borderRadius: 10,
+            backgroundColor: "#000180",
+            padding: "18px 36px",
+            fontSize: "18px",
+          }}
+          variant="contained"
+          sx={{
+            width: { sm: "100%", md: "15%" },
+            textAlign: { sm: "left", md: "center" },
+            display: "inline-flex",
+            ml: "7%",
+            mt: "2%",
+          }}
+        >
+          Apply now
+        </Button>
         <Box
           component="img"
           sx={{
-            height: 233,
-            width: 350,
-            maxHeight: { xs: 233, md: 167 },
-            maxWidth: { xs: 350, md: 250 },
+            height: "233px",
+            width: "350px",
+            maxHeight: { xs: 233, md: 267 },
+            maxWidth: { xs: 350, md: 450 },
+            alignItems: "right",
+            display: "inline-flex",
+            ml: "75%",
           }}
           alt="Car"
-          src="car clear background.png"
+          src="/assets/images/car clear background.png"
         />
-        <img src="/assets/images/car clear background.png" />
       </Paper>
     </AppBar>
   );
