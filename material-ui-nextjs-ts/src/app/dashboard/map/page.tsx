5d5b9e9461a43122b339f46d2bb8b34558c8650f
yoissy <33407132+yoissy@users.noreply.github.com>
2024-03-20 14:19:12 -0400

    Pigeon Map Work

--- material-ui-nextjs-ts/src/app/dashboard/map/page.tsx
+++ material-ui-nextjs-ts/src/app/dashboard/map/page.tsx
@@ -1,14 +1,23 @@
+//needed for maps
+'use client'
+
 import * as React from "react";
 import { useEffect } from "react";
-import { Map, Marker} from "pigeon-maps"
+import { Map, Marker, Overlay} from "pigeon-maps"
 import { osm } from 'pigeon-maps/providers'
 import { Metadata } from 'next'
 import { useState } from "react";
+import { CarRental } from "@mui/icons-material";
+
 
 // set page metadata
+/* 
+* doesn't work, causing errors
+* will need to find another way to do this
 export const metadata: Metadata = {
     title: 'Map',
   }
+*/
 
 
 
@@ -16,12 +25,8 @@ export default function MapPage() {
     //default map props
     
     
-    const [center, setCenter] = useState([86, 39.13]);
-    const zoom = useState(15);
-
-    useEffect(() => {
-        setCenter([1010.99835602, 77.01502627]);
-      }, [1010.99835602, 77.01502627]);
+    const [center, setCenter] = useState([50.879, 4.6997])
+    const [zoom, setZoom] = useState(11)
 
     return (
       <React.Fragment>
@@ -31,12 +36,38 @@ export default function MapPage() {
         <h4>
             This is a placeholder
         </h4>
-        <div style={{ height: "90vh", width: "100%" }}>
-            <Map center={center} zoom={zoom}>
-                <Marker width={50} anchor={center} />
-            </Map>
-                {latitude} : {longitude}
-        </div>
+        <Map 
+            height={500}
+            center={[50.879, 4.6997]} 
+            zoom={zoom} 
+            onBoundsChanged={({ center, zoom }) => { 
+                //setCenter(center) 
+                setZoom(zoom) 
+            }} 
+        >
+            {/* marker example 1 */}
+            <Marker 
+                width={50}
+                anchor={[50.890, 4.71]} 
+                //color={red} 
+                //onClick={() => setHue(hue + 20)} 
+            />
+            {/* marker example 2 */}
+            <Marker 
+                width={50}
+                anchor={[50.879, 4.6997]} 
+                //color={color} 
+                //onClick={() => setHue(hue + 20)} 
+            >
+                <CarRental />
+            </Marker>
+            {/* marker example 3 - overlay */}
+            {/* you can use this to add any react element you want */}
+            {/* in theory examples 2 and 3 are anchored to the same point. Note how the way they scale is strange. Will have to figure that out */}
+            <Overlay anchor={[50.879, 4.6997]} offset={[120, 79]}>
+                <img src='https://pigeon-maps.js.org/img/pigeon.jpg' width={30} height={30} alt='' />
+            </Overlay>
+        </Map>
       </React.Fragment>
     );
   }
--- material-ui-nextjs-ts/src/app/page.tsx
+++ material-ui-nextjs-ts/src/app/page.tsx
@@ -1,37 +1,18 @@
-//this is needed because some mui functions expecet "client" things like being able to use hooks.
-"use client";
+import React, { useState } from 'react'
+import { Map } from 'pigeon-maps'
 
-import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import NextLink from "next/link";
-import Copyright from "@/components/Dashboard Components/Copyright";
-import ResponsiveAppBar from "@/components/ResponseiveAppBar";
-import FAQ from "@/components/FAQ";
-import GyroHero from "@/components/GyroHero";
-import Testimonials from "@/components/Testimonials";
-import AboutUs from "@/components/AboutUs";
-export default function Home() {
+export function MyMap() {
+  const [center, setCenter] = useState([50.879, 4.6997])
+  const [zoom, setZoom] = useState(11)
   return (
-    <React.Fragment>
-      <ResponsiveAppBar></ResponsiveAppBar>
-      <GyroHero />
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Testimonials />
-        <AboutUs />
-        <FAQ />
-        <Copyright />
-      </Box>
-    </React.Fragment>
-  );
+    <Map 
+      height={300}
+      center={center} 
+      zoom={zoom} 
+      onBoundsChanged={({ center, zoom }) => { 
+        setCenter(center) 
+        setZoom(zoom) 
+      }} 
+    />
+  )
 }
