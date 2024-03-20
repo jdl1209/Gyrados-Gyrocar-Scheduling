f464c19ccd37d4e107ce63817c7f943e2aecaabc
yoissy <33407132+yoissy@users.noreply.github.com>
2024-03-20 17:48:06 -0400

    Update page.tsx

--- material-ui-nextjs-ts/src/app/page.tsx
+++ material-ui-nextjs-ts/src/app/page.tsx
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
-import { Map } from 'pigeon-maps'
+//this is needed because some mui functions expecet "client" things like being able to use hooks.
+"use client";
 
-export function MyMap() {
-  const [center, setCenter] = useState([50.879, 4.6997])
-  const [zoom, setZoom] = useState(11)
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import NextLink from "next/link";
+import Copyright from "@/components/Dashboard Components/Copyright";
+import ResponsiveAppBar from "@/components/ResponseiveAppBar";
+import FAQ from "@/components/FAQ";
+import GyroHero from "@/components/GyroHero";
+import Testimonials from "@/components/Testimonials";
+import AboutUs from "@/components/AboutUs";
+
+export default function Home() {
   return (
-    <Map 
-      height={300}
-      center={center} 
-      zoom={zoom} 
-      onBoundsChanged={({ center, zoom }) => { 
-        setCenter(center) 
-        setZoom(zoom) 
-      }} 
-    />
-  )
+    <React.Fragment>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <GyroHero />
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Testimonials />
+        <AboutUs />
+        <FAQ />
+        <Copyright />
+      </Box>
+    </React.Fragment>
+  );
 }
