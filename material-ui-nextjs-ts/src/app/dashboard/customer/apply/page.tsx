ca17906d2de1ca7471a71586ee423f6d63cb3a84
yoissy <33407132+yoissy@users.noreply.github.com>
2024-03-25 13:28:50 -0400

    Relocated Apply Page & Added Global Todo File

--- material-ui-nextjs-ts/src/app/dashboard/customer/apply/page.tsx
+++ material-ui-nextjs-ts/src/app/dashboard/customer/apply/page.tsx
@@ -1,138 +1,6 @@
 // //this is needed because some mui functions expecet "client" things like being able to use hooks. In this case we need it for the theming
 "use client";
 
-// import * as React from 'react';
-// import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
-// import Link from '@mui/material/Link';
-// import Grid from '@mui/material/Grid';
-// import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import ResponsiveAppBar from '@/components/ResponsiveAppBar';
-
-// function Copyright(props: any) {
-//   return (
-//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
-
-// export default function SignUp() {
-//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     const data = new FormData(event.currentTarget);
-//     console.log({
-//       email: data.get('email'),
-//       password: data.get('password'),
-//     });
-//   };
-
-//   return (
-//     //TODO: remove this themeprovider? It should already be provided by layout
-//     //Also idk what purpose the component="main" container has
-//     <Container disableGutters>
-//       <ResponsiveAppBar></ResponsiveAppBar>
-//       <Container component="main" maxWidth="xs">
-//         <Box
-//           sx={{
-//             marginTop: 8,
-//             display: 'flex',
-//             flexDirection: 'column',
-//             alignItems: 'center',
-//           }}
-//         >
-//           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-//             <LockOutlinedIcon />
-//           </Avatar>
-//           <Typography component="h1" variant="h5">
-//             Sign up
-//           </Typography>
-//           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-//             <Grid container spacing={2}>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                   autoComplete="given-name"
-//                   name="firstName"
-//                   required
-//                   fullWidth
-//                   id="firstName"
-//                   label="First Name"
-//                   autoFocus
-//                 />
-//               </Grid>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="lastName"
-//                   label="Last Name"
-//                   name="lastName"
-//                   autoComplete="family-name"
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="email"
-//                   label="Email Address"
-//                   name="email"
-//                   autoComplete="email"
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   name="password"
-//                   label="Password"
-//                   type="password"
-//                   id="password"
-//                   autoComplete="new-password"
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <FormControlLabel
-//                   control={<Checkbox value="allowExtraEmails" color="primary" />}
-//                   label="I want to receive inspiration, marketing promotions and updates via email."
-//                 />
-//               </Grid>
-//             </Grid>
-//             <Button
-//               type="submit"
-//               fullWidth
-//               variant="contained"
-//               sx={{ mt: 3, mb: 2 }}
-//             >
-//               Sign Up
-//             </Button>
-//             <Grid container justifyContent="flex-end">
-//               <Grid item>
-//                 <Link href="/signin" variant="body2">
-//                   Already have an account? Sign in
-//                 </Link>
-//               </Grid>
-//             </Grid>
-//           </Box>
-//         </Box>
-//       </Container>
-//     <Copyright sx={{ mt: 5 }} />
-//     </Container>
-//   );
-// }
-
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import zxcvbn from "zxcvbn";
@@ -170,7 +38,7 @@ interface IFormInput {
   licenseNumber: string;
 }
 
-const SignUp: React.FC = async () => {
+const Apply: React.FC = async () => {
   const {
     register,
     handleSubmit,
@@ -199,10 +67,10 @@ const SignUp: React.FC = async () => {
 
   return (
     <>
-      <ResponsiveAppBar isSignedIn={isSignedIn}></ResponsiveAppBar>
-      <ThemeProvider theme={createTheme()}>
+
+
         <Grid container component="main" sx={{ height: "100vh" }}>
-          <CssBaseline />
+
           <Grid
             item
             xs={false}
@@ -316,6 +184,7 @@ const SignUp: React.FC = async () => {
                   label="Driver's License ID"
                   autoComplete="licenseNumber"
                 />
+                {/* TODO @Kevin can you fix up this page to remove things like the email and password since this is now accessed from the login */}
                 <TextField
                   {...register("email")}
                   margin="normal"
@@ -383,9 +252,9 @@ const SignUp: React.FC = async () => {
             </Box>
           </Grid>
         </Grid>
-      </ThemeProvider>
+
     </>
   );
 };
 
-export default SignUp;
+export default Apply;
