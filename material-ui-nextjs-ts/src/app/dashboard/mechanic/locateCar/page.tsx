cfbdd8e4e3d071a69c00f9c7d770fe3c4fb5b0e5
kevros167 <56698152+kevros167@users.noreply.github.com>
2024-04-03 22:08:26 -0400

    Locate Car Page

--- material-ui-nextjs-ts/src/app/dashboard/mechanic/locateCar/page.tsx
+++ material-ui-nextjs-ts/src/app/dashboard/mechanic/locateCar/page.tsx
@@ -1,24 +1,183 @@
-import React from "react";
+// needed for maps
+"use client";
 
-export default function LocateCar() {
-    // this will show a way to locate a car in the system
+import React, { useEffect, useState } from "react";
+import { Map, Marker, ZoomControl } from "pigeon-maps";
+import axios from "axios";
+//import { Metadata } from 'next'
 
-    //this will check if the person has permisions based on who they're logged in as
-    //TODO implement check
-    if (true) {
-        return (
-            <React.Fragment>
-              <h1>
+// set page metadata
+// export const metadata: Metadata = {
+//     title: 'Map',
+// }
+
+const LocateCar = () => {
+  // this will show a way to locate a car in the system
+  const [devices, setDevices] = useState([]);
+  const [positions, setPositions] = useState([]);
+  const [center, setCenter] = useState([43.137726, -77.64232]);
+  const [zoom, setZoom] = useState(11);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token =
+          "RjBEAiBdIdHoUcbQmwqHtQGFH9Th4SRyiVTb1lu446q2maSYMgIgViqHSudIAoGreLQiAmmw9ccFb-ChyHpa2lZRzJSFnoh7InUiOjEsImUiOiIyMDI0LTA1LTMwVDA0OjAwOjAwLjAwMCswMDowMCJ9";
+
+        const devicesResponse = await axios.get(
+          "http://23.20.214.210:8082/api/devices",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log("Response data:", devicesResponse.data); // Log response data
+
+        const positionsResponse = await axios.get(
+          "http://23.20.214.210:8082/api/positions",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log("Response data:", positionsResponse.data); // Log response data
+        setDevices(devicesResponse.data);
+        setPositions(positionsResponse.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Combine devices and positions into an array of objects containing both device and position information
+  const combinedData = devices.map((device, index) => ({
+    id: device.id,
+    name: device.name,
+    latitude: positions[index] ? positions[index].latitude : null,
+    longitude: positions[index] ? positions[index].longitude : null,
+    position: positions[index], // Access position information based on index
+  }));
+
+  //this will check if the person has permisions based on who they're logged in as
+  //TODO implement check
+  if (true) {
+    return (
+      <React.Fragment>
+        {/* <h1>
                   Locate Car Page
               </h1>
               <h4>
                   This is a placeholder
-              </h4>
-            </React.Fragment>
-          );
-    } else {
-        return (
-            <div />
-        )
-    }
+              </h4> */}
+        <h1 style={{ textAlign: "center" }}>Car Locator Map</h1>
+        <Map
+          height={"110vh"}
+          center={center}
+          zoom={zoom}
+          metaWheelZoom={true} //causes hydration error
+          onBoundsChanged={({ center, zoom }) => {
+            setCenter(center);
+            setZoom(zoom);
+          }}
+        >
+          <ZoomControl />
+          {combinedData.map(({ id, name, latitude, longitude, position }) => (
+            <Marker
+              key={id}
+              anchor={[latitude, longitude]}
+              onClick={() => console.log("Clicked on device:", name)}
+              color="#33adad"
+            />
+          ))}
+          <Marker
+            //width={50}
+            anchor={[43.20663, -77.68602]}
+            color="#000180"
+          >
+            <div
+              style={{
+                backgroundColor: "white",
+                padding: "px",
+                borderRadius: "5px",
+                fontSize: "12px",
+              }}
+            >
+              {"GyroGoGo Northwest"}
+            </div>
+          </Marker>
+          <Marker
+            //width={50}
+            anchor={[43.21223, -77.45218]}
+            color="#000180"
+          >
+            <div
+              style={{
+                backgroundColor: "white",
+                padding: "1px",
+                borderRadius: "5px",
+                fontSize: "12px",
+              }}
+            >
+              {"GyroGoGo Northeast"}
+            </div>
+          </Marker>
+          <Marker
+            //width={50}
+            anchor={[43.15752, -77.61197]}
+            color="#000180"
+          >
+            <div
+              style={{
+                backgroundColor: "white",
+                padding: "1px",
+                borderRadius: "5px",
+                fontSize: "12px",
+              }}
+            >
+              {"GyroGoGo Center City"}
+            </div>
+          </Marker>
+          <Marker
+            //width={50}
+            anchor={[43.06997, -77.44159]}
+            color="#000180"
+          >
+            <div
+              style={{
+                backgroundColor: "white",
+                padding: "1px",
+                borderRadius: "5px",
+                fontSize: "12px",
+              }}
+            >
+              {"GyroGoGo Southeast"}
+            </div>
+          </Marker>
+          <Marker
+            //width={5}
+            anchor={[43.10884, -77.67537]}
+            color="#000180"
+          >
+            <div
+              style={{
+                backgroundColor: "white",
+                padding: "1px",
+                borderRadius: "5px",
+                fontSize: "12px",
+              }}
+            >
+              {"GyroGoGo Airport"}
+            </div>
+          </Marker>
+        </Map>
+      </React.Fragment>
+    );
+  } else {
+    return <div />;
   }
+};
+export default LocateCar;
