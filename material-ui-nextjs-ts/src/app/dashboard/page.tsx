c5355b20b1f3be1d7eb9c97c99ce88282d92974e
Erich <89471455+SnellsCorner@users.noreply.github.com>
2024-04-07 16:36:37 -0400

    Updated role functionality to get the role of the current user

--- material-ui-nextjs-ts/src/app/dashboard/page.tsx
+++ material-ui-nextjs-ts/src/app/dashboard/page.tsx
@@ -1,13 +1,65 @@
-'use client'
-
+// Import necessary modules
 import AdminDashboard from "@/components/AdminDashboard";
 import CustomerDashboard from "@/components/CustomerDashboard";
-
-//curently this is a blank page, might never be used, might set it up as a reference to another page as a fallback
-//TODO: Set as reference to whichever page we want to be the default maybe?
+import { DB } from "@/lib/db";
+import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { Box, Typography } from "@mui/material";
+import DashboardLayout from "./layout";
 
 // `app/page.tsx` is the UI for the `/` URL
-export default function Page() {
-    //return <CustomerDashboard></CustomerDashboard>
-    //return <AdminDashboard></AdminDashboard>
+// Wrap the component with `withPageAuthRequired` to require authentication
+const DashboardPage = async () => {
+  const session = await getSession();
+  
+  if (!session || !session.user) {
+    return (
+      <Box sx={{ p: 2, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Typography variant="h6">Invalid session</Typography>
+      </Box>
+    );
   }
+
+  console.log(session.user.sub);
+
+  const user = session.user;
+  const userRoles: string[] = session.user.roles || [];
+
+  console.log('User roles:', userRoles);
+  console.log('Session:', session);
+
+  const db = new DB();
+  
+  try {
+    // Await the resolution of the Promise returned by db.getUserRole
+    const role = await db.getUserRole(session.user.sub);
+
+    // Check if the role is of type string array
+    if (Array.isArray(role)) {
+      console.log("This is my user role " + role);
+      return (
+        <div>
+          {/* Pass the user's role to the layout component */}
+          <DashboardLayout role={role} children={undefined} />
+        </div>
+      );
+    } else {
+      console.error("User role is not of type string array");
+      return (
+        <Box sx={{ p: 2, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Typography variant="h6">Error fetching user role</Typography>
+        </Box>
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching user role:", error);
+    return (
+      <Box sx={{ p: 2, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Typography variant="h6">Error fetching user role</Typography>
+      </Box>
+    );
+  }
+}
+
+export default withPageAuthRequired(DashboardPage);
+
+
