8f478e098b272caadff0f5297bd840fbec20629b
yoissy <33407132+yoissy@users.noreply.github.com>
2024-04-09 10:26:40 -0400

    Update database.sql

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS ROCHESTER;
 CREATE DATABASE ROCHESTER;
 USE ROCHESTER;
 
+SET SQL_SAFE_UPDATES = 0;
+
 /*
 TABLE roles
 stores role information
@@ -14,95 +16,56 @@ CREATE TABLE roles(
 );
 
 /*
-TABLE customer
-stores all customer information
-PK - customerID (auto incrementing)
-FK - roleID (roles)
+TABLE user
+stores information for all of our users, userID will be the ID that is stored in our login service
+PK - customerID
+FK - rolesID
 */
-CREATE TABLE customer(
-    customerID INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE users(
+    userID VARCHAR(100) PRIMARY KEY,
+    roleID SMALLINT NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     fName VARCHAR(20) NOT NULL,
     mInitial VARCHAR(1),
     lName VARCHAR(50) NOT NULL,
     suffix VARCHAR(10),
-    phoneNum VARCHAR(10) NOT NULL,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    phoneNum VARCHAR(10),
     email VARCHAR(50) NOT NULL,
-    address1 VARCHAR(50) NOT NULL,
+    address1 VARCHAR(50) ,
     address2 VARCHAR (50),
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(2) NOT NULL,
-    zip VARCHAR(5) NOT NULL,
+    city VARCHAR(50),
+    state VARCHAR(2),
+    zip VARCHAR(5),
     activated SMALLINT,
-    roleID SMALLINT,
-    FOREIGN KEY(roleID) REFERENCES roles(roleID)
-);
-
-/*
-TABLE customer_credit_info
-stores hashed information of the customers credit car and drivers license
-PK - customerID
-FK - customerID (customers)
-*/
-CREATE TABLE customer_credit_info(
-	customerID INT PRIMARY KEY,
-    hashedCreditNumber VARCHAR(200) NOT NULL,
-    hashedSecurity VARCHAR(200) NOT NULL,
-    hashedZipcode VARCHAR(200) NOT NULL,
-    hashedExpiration VARCHAR(200) NOT NULL,
-    hashedDriversID VARCHAR (200) NOT NULL,
-    FOREIGN KEY(customerID) REFERENCES customer(customerID)
-);
-
-/*
-TABLE customer_password
-stores customer hashed password (no looking mr FBI agent)
-PK - customerID
-FK - customerID (customers)
-*/
-CREATE TABLE customer_password(
-	customerID INT PRIMARY KEY,
-    hashedPass VARCHAR(200) NOT NULL,
-    FOREIGN KEY (customerID) REFERENCES customer(customerID) 
-);
-
-/*
-TABLE suspended_users
-stores users who are suspended
-PK - employeeID
-FK - roleID(roles)
-*/
-CREATE TABLE suspended_users(
-	userID INT PRIMARY KEY,
-    suspended VARCHAR (3) NOT NULL,
-    FOREIGN KEY (userID) REFERENCES customer(customerID)
-);
-
-/*
-TABLE employees
-stores employee information like office and username and role
-PK - employeeID
-FK - roleID(roles)
-*/
-CREATE TABLE employees(
-	employeeID INT PRIMARY KEY AUTO_INCREMENT,
-    roleID SMALLINT NOT NULL,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    fullname VARCHAR(100) NOT NULL,
     office VARCHAR(50),
     FOREIGN KEY (roleID) REFERENCES roles(roleID)
 );
 
 /*
-TABLE employee_password
-stores employees hashed password (no looking mr FBI agent)
-PK - employeeID
-FK - roleID(roles)
+TABLE customer_credit_info
+stores hashed information of the customers credit car and drivers license
+PK - userID
+FK - userID
 */
-CREATE TABLE employee_password(
-	employeeID INT PRIMARY KEY,
-    hashedPass VARCHAR(200) NOT NULL,
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID)
+CREATE TABLE customer_credit_info(
+	userID VARCHAR(100) PRIMARY KEY,
+    hashedCreditNumber VARCHAR(200) NOT NULL,
+    hashedSecurity VARCHAR(200) NOT NULL,
+    hashedZipcode VARCHAR(200) NOT NULL,
+    hashedExpiration VARCHAR(200) NOT NULL,
+    hashedDriversID VARCHAR (200) NOT NULL,
+    FOREIGN KEY(userID) REFERENCES users(userID)
+);
+
+/*
+TABLE suspended_users
+stores users who are suspended
+PK - userID
+FK - userID
+*/
+CREATE TABLE suspended_users(
+	userID VARCHAR(100) PRIMARY KEY,
+    FOREIGN KEY (userID) REFERENCES users(userID)
 );
 
 /*
@@ -111,9 +74,9 @@ stores data for locations for each car stop
 PK - sublocationID
 */
 CREATE TABLE location(
-	sublocationID INT PRIMARY KEY AUTO_INCREMENT,
+	locationID INT PRIMARY KEY AUTO_INCREMENT,
     locationName VARCHAR(400) NOT NULL UNIQUE,
-    sublocationName VARCHAR(100) NOT NULL UNIQUE,
+    locationArea VARCHAR(100) NOT NULL UNIQUE,
     address VARCHAR(400),
     cityName VARCHAR (30) NOT NULL,
     state VARCHAR(2) NOT NULL,
@@ -129,12 +92,12 @@ FK - sublocationID
 */
 CREATE TABLE cars(
 	carID INT PRIMARY KEY AUTO_INCREMENT,
-    carType VARCHAR(20) NOT NULL,
+    carType VARCHAR(20),
     battery DECIMAL(5,2) NOT NULL,
     status VARCHAR(20),
     reserved SMALLINT NOT NULL,
-    sublocationID INT NOT NULL,
-    FOREIGN KEY (sublocationID) REFERENCES location(sublocationID)
+    currentLocationID INT NOT NULL,
+    FOREIGN KEY (currentLocationID) REFERENCES location(locationID)
 );
 
 /*
@@ -145,49 +108,13 @@ FK - carID
 */
 CREATE TABLE jobs(
 	taskID INT AUTO_INCREMENT PRIMARY KEY,
-    employeeID INT,
+    userID VARCHAR(100),
     carID INT NOT NULL,
     task VARCHAR(20) NOT NULL,
     notes VARCHAR(500) NULL,
     jTimeDate DATETIME NOT NULL,
     FOREIGN KEY (carID) REFERENCES cars(carID),
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID)
-);
-
-/*
-TABLE reserved
-stores information for each car that is reserved
-PK - carID
-FK - customerID, sublocationID
-*/
-CREATE TABLE reserved(
-	carID INT PRIMARY KEY,
-    customerID INT NOT NULL,
-    sublocationID INT NOT NULL,
-    rTime TIME NOT NULL,
-    rDate DATE NOT NULL,
-    FOREIGN KEY (carID) REFERENCES cars(carID),
-    FOREIGN KEY (sublocationID) REFERENCES location(sublocationID)
-);
-
-/*
-TABLE reservation_history
-stores information of the reservation history for each car
-PK - customerID
-FK - carID, sublocationIDt, sublocationIDr
-*/
-CREATE TABLE reservation_history(
-	customerID INT PRIMARY KEY,
-    carID INT NOT NULL,
-    timeTaken TIME NOT NULL,
-    timeReturned TIME NOT NULL,
-    dateTaken DATE NOT NULL,
-    dateReturned DATE NOT NULL,
-    sublocationIDt INT NOT NULL,
-    sublocationIDr INT NOT NULL,
-    FOREIGN KEY (carID) REFERENCES cars(carID),
-    FOREIGN KEY (sublocationIDt) REFERENCES location(sublocationID),
-    FOREIGN KEY (sublocationIDr) REFERENCES location(sublocationID)
+    FOREIGN KEY (userID) REFERENCES users(userID)
 );
 
 /*
@@ -198,11 +125,11 @@ FK - carID, sublocationID
 */
 CREATE TABLE gps_location (
 	carID INT PRIMARY KEY,
-    sublocationID INT,
+    locationID INT,
     gpsLat DOUBLE NOT NULL,
     gpsLong DOUBLE NOT NULL,
     FOREIGN KEY (carID) REFERENCES cars(carID),
-    FOREIGN KEY (sublocationID) REFERENCES location(sublocationID)
+    FOREIGN KEY (locationID) REFERENCES location(locationID)
 );
 
 /*
@@ -215,49 +142,110 @@ CREATE TABLE faq (
     faqID INT PRIMARY KEY AUTO_INCREMENT,
     faqQuestion VARCHAR (500),
     faqAnswer VARCHAR (500),
-    employeeAdded INT,
-    FOREIGN KEY (employeeAdded) REFERENCES employees(employeeID)
-);
-
-/*
-TABLE calendar
-stores the calendar id and number and days associated with that calendar
-PK - calendarID
-*/
-CREATE TABLE calendar(
-    calendarID INT PRIMARY KEY AUTO_INCREMENT,
-    calendarNum INT NOT NULL,
-    day DATE NOT NULL
-);
-
-/*
-TABLE sublocation_calendar
-lookup table for calendar and sublocation
-PK - sublocationID
-FK - sublocationID, calendarNum
-*/
-CREATE TABLE sublocation_calendar (
-    sublocationID INT,
-    calendarID INT,
-    FOREIGN KEY (sublocationID) REFERENCES location(sublocationID),
-    FOREIGN KEY (calendarID) REFERENCES calendar(calendarID)
+    userID VARCHAR(100),
+    FOREIGN KEY (userID) REFERENCES users(userID)
 );
 
 /*
 TABLE reservation
 stores the reservation for the car in the calendar
 PK - reservationID
-FK - customerID, carID, calendarNum, day
+FK - customerID, carID, locationID, locationIDToReturn
 */
 CREATE TABLE reservation (
     reservationID INT PRIMARY KEY AUTO_INCREMENT,
-    customerID INT NOT NULL,
+    userID VARCHAR(100) NOT NULL,
     carID INT NOT NULL,
-    calendarID INT NOT NULL,
-    day DATE NOT NULL,
+    dateCreated INT NOT NULL,
+    locationID INT NOT NULL,
+    locationIDToReturn INT NOT NULL,
     timeBegin DATETIME NOT NULL,
     timeEnd DATETIME NOT NULL,
-    FOREIGN KEY (customerID) REFERENCES customer(customerID),
+    paid BOOLEAN NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(userID),
     FOREIGN KEY (carID) REFERENCES cars(carID),
-    FOREIGN KEY (calendarID) REFERENCES calendar(calendarID)
+    FOREIGN KEY (locationID) REFERENCES location(locationID),
+    FOREIGN KEY (locationIDToReturn) REFERENCES location(locationID)
 );
+
+/*
+TABLE days
+stores the days that are available for booking
+PK - dayID
+*/
+CREATE TABLE days(
+	dayID INT PRIMARY KEY AUTO_INCREMENT,
+    day DATE NOT NULL
+);
+
+/*
+TABLE price
+stores the prices for Gyrocar reservation
+PK - priceID
+*/
+CREATE TABLE price(
+    priceID INT PRIMARY KEY AUTO_INCREMENT,
+    priceName VARCHAR(80),
+    priceInCents BIGINT
+);
+
+DELIMITER $$
+/*
+TABLE days
+insert a given day into the days table
+PK - dayID
+*/
+CREATE PROCEDURE insertDays(
+	dt DATE
+)
+BEGIN
+   INSERT INTO days(day)
+   VALUES(dt);
+END$$
+
+DELIMITER $$
+/*
+STORED PROCEDURE loadDays
+A procedure to call loadDays, which will while loop the amount of days and call the insertDays procedure
+*/
+CREATE PROCEDURE loadDays(
+   startDate DATE,
+   day INT
+)
+BEGIN
+   DECLARE counter INT DEFAULT 1;
+   DECLARE dt DATE DEFAULT startDate;
+   WHILE counter <= day DO
+       CALL insertDays(dt);
+       SET counter = counter + 1;
+       SET dt = DATE_ADD(dt,INTERVAL 1 day);
+   END WHILE;
+END$$
+DELIMITER ;
+
+/*
+STORED PROCEDURE addRemoveDays
+A procedure to add the 31st day and remove the previous day
+*/
+DELIMITER $$
+CREATE PROCEDURE addRemoveDays(
+	yesterday DATE,
+    31stDate DATE
+)
+BEGIN
+	DELETE FROM days WHERE day = yesterday;
+    INSERT INTO days(day) VALUES (31stDate);
+END$$
+DELIMITER ;
+
+/*
+EVENT addRemoveDays
+An event that will update the database removing the previous day and 
+*/
+CREATE EVENT addRemoveDays
+    ON SCHEDULE EVERY 1 DAY
+    STARTS '2024-03-24 00:00:01'
+    DO
+      SELECT DATE_SUB(CURDATE(), INTERVAL 1 DAY) INTO @yesterday_date;
+	  SELECT DATE_ADD(CURDATE(), INTERVAL 30 DAY) INTO @thirtydays;
+	  CALL addRemoveDays(@yesterday_date, @thirtydays);
