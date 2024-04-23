cc8d29a1a639ccbe1293cec4c919facedce719fd
jdl1209 <54865107+jdl1209@users.noreply.github.com>
2024-04-22 21:27:35 -0400

    Update database.sql

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -101,20 +101,24 @@ CREATE TABLE cars(
 );
 
 /*
-TABLE jobs
-stores information on each job done to a car
-PK - employeeID
-FK - carID
+TABLE mechanic_reports
+Stores the reports that mechanics make for the cars. Reports can range from anything to 
+simple check ups, fatal errors, routine maintenance, etc.
+PK - reportID
+FK - carID, locationID
 */
-CREATE TABLE jobs(
-	taskID INT AUTO_INCREMENT PRIMARY KEY,
-    userID VARCHAR(100),
+
+CREATE TABLE mechanic_reports(
+    reportID INT PRIMARY KEY AUTO_INCREMENT,
+    reportType ENUM('Service Report','Status Report','Damage Report','Miscellaneous') NOT NULL,
     carID INT NOT NULL,
-    task VARCHAR(20) NOT NULL,
-    notes VARCHAR(500) NULL,
-    jTimeDate DATETIME NOT NULL,
+    locationID INT NOT NULL,
+    reportStatus VARCHAR(20) NOT NULL,
+    timeSpentLabor TIME NOT NULL,
+    tasks VARCHAR(255) NOT NULL,
+    notes VARCHAR(255) NOT NULL,
     FOREIGN KEY (carID) REFERENCES cars(carID),
-    FOREIGN KEY (userID) REFERENCES users(userID)
+    FOREIGN KEY (locationID) REFERENCES location(locationID)
 );
 
 /*
@@ -169,16 +173,6 @@ CREATE TABLE reservation (
 );
 
 /*
-TABLE days
-stores the days that are available for booking
-PK - dayID
-*/
-CREATE TABLE days(
-	dayID INT PRIMARY KEY AUTO_INCREMENT,
-    day DATE NOT NULL
-);
-
-/*
 TABLE price
 stores the prices for Gyrocar reservation
 PK - priceID
@@ -189,84 +183,3 @@ CREATE TABLE price(
     priceInCents BIGINT
 );
 
-DELIMITER $$
-/*
-TABLE days
-insert a given day into the days table
-PK - dayID
-*/
-CREATE PROCEDURE insertDays(
-	dt DATE
-)
-BEGIN
-   INSERT INTO days(day)
-   VALUES(dt);
-END$$
-
-DELIMITER $$
-/*
-STORED PROCEDURE loadDays
-A procedure to call loadDays, which will while loop the amount of days and call the insertDays procedure
-*/
-CREATE PROCEDURE loadDays(
-   startDate DATE,
-   day INT
-)
-BEGIN
-   DECLARE counter INT DEFAULT 1;
-   DECLARE dt DATE DEFAULT startDate;
-   WHILE counter <= day DO
-       CALL insertDays(dt);
-       SET counter = counter + 1;
-       SET dt = DATE_ADD(dt,INTERVAL 1 day);
-   END WHILE;
-END$$
-DELIMITER ;
-
-/*
-STORED PROCEDURE addRemoveDays
-A procedure to add the 31st day and remove the previous day
-*/
-DELIMITER $$
-CREATE PROCEDURE addRemoveDays(
-	yesterday DATE,
-    31stDate DATE
-)
-BEGIN
-	DELETE FROM days WHERE day = yesterday;
-    INSERT INTO days(day) VALUES (31stDate);
-END$$
-DELIMITER ;
-
-/*
-EVENT addRemoveDays
-An event that will update the database removing the previous day and 
-*/
-CREATE EVENT addRemoveDays
-    ON SCHEDULE EVERY 1 DAY
-    STARTS '2024-03-24 00:00:01'
-    DO
-      SELECT DATE_SUB(CURDATE(), INTERVAL 1 DAY) INTO @yesterday_date;
-	  SELECT DATE_ADD(CURDATE(), INTERVAL 30 DAY) INTO @thirtydays;
-	  CALL addRemoveDays(@yesterday_date, @thirtydays);
-
-/*
-TABLE reports
-Stores the reports that mechanics make for the cars. Reports can range from anything to 
-simple check ups, fatal errors, routine maintenance, etc.
-PK - reportID
-FK - carID, sublocationID
-*/
-
-CREATE TABLE reports (
-    reportID INT PRIMARY KEY AUTO_INCREMENT,
-    reportType ENUM('Service Report','Status Report','Damage Report','Miscellaneous') NOT NULL,
-    carID INT NOT NULL,
-    sublocationID INT NOT NULL,
-    reportStatus VARCHAR(20) NOT NULL,
-    timeSpentLabor VARCHAR(6) NOT NULL,
-    tasks VARCHAR(255) NOT NULL,
-    notes VARCHAR(255) NOT NULL,
-    FOREIGN KEY (carID) REFERENCES cars(carID)
-    -- FOREIGN KEY (sublocationID) REFERENCES location(sublocationID)
-);
